a59558942b90301aeba172c009f136ad680c7c41
zii <zhangjianjin@iyunmai.com>
2018-01-02 21:50:49 +0800

    1.配置productFlavor 2.指定输出apk的名称

--- app/src/main/java/com/zii/study/gradlestudy/MainActivity.java
+++ app/src/main/java/com/zii/study/gradlestudy/MainActivity.java
@@ -2,6 +2,7 @@ package com.zii.study.gradlestudy;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView) findViewById(R.id.tv);
+        tv.setText("appId:    " + BuildConfig.APPLICATION_ID
+            + "\n\nbuildType:    " + BuildConfig.BUILD_TYPE
+            + "\n\nflavor:    " + BuildConfig.FLAVOR
+            + "\n\nversionName:    " + BuildConfig.VERSION_NAME
+            + "\n\nversionCode:    " + BuildConfig.VERSION_CODE
+            + "\n\nDebug:    " + BuildConfig.DEBUG
+        );
     }
 }
